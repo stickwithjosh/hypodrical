95cd8d18b3880348f7c41d37c554c524c0d962fd
Joshua Blount <hello@joshuablount.com>
2013-02-22 13:48:47 -0500

    impolite specific stuff

--- hypodrical/settings.py
+++ hypodrical/settings.py
@@ -126,9 +126,9 @@ if production:
     COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
     # boto settings
-    AWS_ACCESS_KEY_ID = 'XXX'
+    AWS_ACCESS_KEY_ID = 'AKIAIT3JEWHLY36RVOUQ'
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'XXX'
+    AWS_STORAGE_BUCKET_NAME = 'm.animpolitepodcast.com'
     AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
 
     # S3 URL settings
