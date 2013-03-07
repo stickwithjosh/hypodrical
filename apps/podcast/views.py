df04e3a9fc1738e375323f0d1dac0ee5fc618f67
Bill Evans <bill@squarefactor.com>
2013-03-06 22:05:36 -0500

    episode redirection at root urls now

--- apps/podcast/views.py
+++ apps/podcast/views.py
@@ -1,8 +1,7 @@
 import datetime
 
-from django import http
-from django.views import generic
 from django.views.generic.list_detail import object_list
+from django.shortcuts import get_object_or_404, redirect
 from models import Episode
 
 
@@ -22,20 +21,12 @@ def PodcastFeed(request):
         template_name='podcast/feed.html')
 
 
-class CanonicalDetailView(generic.DetailView):
-    """
-        A DetailView which redirects to the absolute_url, if necessary.
-    """
-    def get_object(self, *args, **kwargs):
-        # Return any previously-cached object
-        if getattr(self, 'object', None):
-            return self.object
-        return super(CanonicalDetailView, self).get_object(*args, **kwargs)
+def canonical_redirect(request, episode_number):
+    """ Look for the episode number passed
+        and redirect to the right url
+        otherwise render a 404
 
-    def get(self, *args, **kwargs):
-        # Make sure to use the canonical URL
-        self.object = self.get_object()
-        obj_url = self.object.get_absolute_url()
-        if self.request.path != obj_url:
-            return http.HttpResponsePermanentRedirect(obj_url)
-        return super(CanonicalDetailView, self).get(*args, **kwargs)
+    """
+
+    episode = get_object_or_404(Episode, episode_number=episode_number)
+    return redirect(episode)
